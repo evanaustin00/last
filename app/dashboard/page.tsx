c821116764b8c410c03afe99a5fe305675a933b7
evanaustin64 <evanaustin64@gmail.com>
2024-12-09 16:57:20 +0700

    last

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -45,7 +45,7 @@ export default async function Page() {
       {/* Latest Transactions */}
       <section className="mt-8">
         <h2 className="text-xl md:text-2xl font-semibold text-green-800 mb-4">
-          Latest Transactions
+          Latest Transactionss
         </h2>
         <div className="bg-white rounded-lg shadow p-4">
           <Suspense fallback={<LatestInvoicesSkeleton />}>
